bdc593b3bf9c3523a16c5934ddcd29668442cf7d
issmith962 <is.smith.962@gmail.com>
2020-02-19 04:55:53 -0700

    Initial Commit

---
+++ app/src/main/java/edu/byu/cs/tweeter/net/request/FollowersRequest.java
@@ -0,0 +1,58 @@
+package edu.byu.cs.tweeter.net.request;
+
+import edu.byu.cs.tweeter.model.domain.User;
+
+/**
+ * Contains all the information needed to make a request to have the server return the next page of
+ * followees for a specified follower.
+ */
+public class FollowersRequest {
+
+    private final User followee;
+    private final int limit;
+    private final User lastFollower;
+
+    /**
+     * Creates an instance.
+     *
+     * @param followee the {@link User} whose followers are to be returned.
+     * @param limit the maximum number of followers to return.
+     * @param lastFollower the last follower that was returned in the previous request (null if
+     *                     there was no previous request or if no followers were returned in the
+     *                     previous request).
+     */
+    public FollowersRequest(User followee, int limit, User lastFollower) {
+        this.followee = followee;
+        this.limit = limit;
+        this.lastFollower = lastFollower;
+    }
+
+    /**
+     * Returns the followee whose followers are to be returned by this request.
+     *
+     * @return the followee.
+     */
+    public User getfollowee() {
+        return followee;
+    }
+
+    /**
+     * Returns the number representing the maximum number of followers to be returned by this request.
+     *
+     * @return the limit.
+     */
+    public int getLimit() {
+        return limit;
+    }
+
+    /**
+     * Returns the last follower that was returned in the previous request or null if there was no
+     * previous request or if no followers were returned in the previous request.
+     *
+     * @return the last follower.
+     */
+    public User getlastFollower() {
+        return lastFollower;
+    }
+}
+
