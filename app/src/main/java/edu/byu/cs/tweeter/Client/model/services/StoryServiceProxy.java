01915fb3fd751fbc6e63ba551bef263e0f19763b
issmith962 <is.smith.962@gmail.com>
2020-11-13 16:59:05 -0700

    Added error handling for null Users (url path can't be created if so)

--- app/src/main/java/edu/byu/cs/tweeter/Client/model/services/StoryServiceProxy.java
+++ app/src/main/java/edu/byu/cs/tweeter/Client/model/services/StoryServiceProxy.java
@@ -6,12 +6,18 @@ import byu.edu.cs.tweeter.shared.model.domain.service.StoryService;
 import byu.edu.cs.tweeter.shared.net.TweeterRemoteException;
 import byu.edu.cs.tweeter.shared.request.StoryRequest;
 import byu.edu.cs.tweeter.shared.response.StoryResponse;
+import edu.byu.cs.tweeter.Client.net.Exception.TweeterRequestException;
 
 public class StoryServiceProxy extends Service implements StoryService {
     public static final String URL_PATH = "/getstory";
     public StoryResponse getStory(StoryRequest request) throws IOException, TweeterRemoteException {
-        String param_alias = "/" + request.getUser().getAlias();
-        String newUrlPath = URL_PATH + param_alias;
-        return getServerFacade().getStory(request, newUrlPath);
+        if (request.getUser() != null) {
+            if ((request.getUser().getAlias() != null) && (!request.getUser().getAlias().equals(""))) {
+                String param_alias = "/" + request.getUser().getAlias();
+                String newUrlPath = URL_PATH + param_alias;
+                return getServerFacade().getStory(request, newUrlPath);
+            }
+        }
+        return new StoryResponse("Failure: User information invalid..");
     }
 }
