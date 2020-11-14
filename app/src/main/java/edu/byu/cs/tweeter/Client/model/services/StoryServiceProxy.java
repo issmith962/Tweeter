e66915e4fa903ae0c1ead27089ab9bcbed845e88
issmith962 <is.smith.962@gmail.com>
2020-11-13 17:14:03 -0700

    Removed error for "" or null alias -- client should ensure alias is never null by using constructors and never setters.

--- app/src/main/java/edu/byu/cs/tweeter/Client/model/services/StoryServiceProxy.java
+++ app/src/main/java/edu/byu/cs/tweeter/Client/model/services/StoryServiceProxy.java
@@ -12,11 +12,9 @@ public class StoryServiceProxy extends Service implements StoryService {
     public static final String URL_PATH = "/getstory";
     public StoryResponse getStory(StoryRequest request) throws IOException, TweeterRemoteException {
         if (request.getUser() != null) {
-            if ((request.getUser().getAlias() != null) && (!request.getUser().getAlias().equals(""))) {
                 String param_alias = "/" + request.getUser().getAlias();
                 String newUrlPath = URL_PATH + param_alias;
                 return getServerFacade().getStory(request, newUrlPath);
-            }
         }
         return new StoryResponse("Failure: User information invalid..");
     }
