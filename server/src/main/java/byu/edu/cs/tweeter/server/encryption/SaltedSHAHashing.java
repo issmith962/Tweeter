2b1746bee25e7768352865a514d8ff20467ae71f
issmith962 <is.smith.962@gmail.com>
2020-12-04 03:49:27 -0700

    Added encryption class for password hashing + salt

---
+++ server/src/main/java/byu/edu/cs/tweeter/server/encryption/SaltedSHAHashing.java
@@ -0,0 +1,50 @@
+package byu.edu.cs.tweeter.server.encryption;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+/*
+Usage:
+    String salt = getSalt();
+    String securePassword = getSecurePassword(<password>, salt);
+    --> store salt and securePassword
+
+    When newPassAttempt is given to the server, run:
+    String newPassEncrypted = getSecurePassword(<newPassAttempt>, salt);
+    if (newPassEncrypted.equals(securePassword)) {
+        you're good to go
+    }
+ */
+
+public class SaltedSHAHashing {
+    private static String getSecurePassword(String password, String salt) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(salt.getBytes());
+            byte[] bytes = md.digest(password.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte aByte : bytes) {
+                sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "FAILED TO HASH PASSWORD";
+    }
+
+    private static String getSalt() {
+        try {
+            SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
+            byte[] salt = new byte[16];
+            sr.nextBytes(salt);
+            return Base64.getEncoder().encodeToString(salt);
+        } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
+            e.printStackTrace();
+        }
+        return "FAILED TO GET SALT";
+    }
+}
