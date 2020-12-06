ccc53a433ae6e1d37f2fbb532e8bf724bf3b2a21
issmith962 <is.smith.962@gmail.com>
2020-12-05 23:55:52 -0700

    Added business logic for getting story from DAO

--- server/src/main/java/byu/edu/cs/tweeter/server/service/StoryServiceImpl.java
+++ server/src/main/java/byu/edu/cs/tweeter/server/service/StoryServiceImpl.java
@@ -1,7 +1,9 @@
 package byu.edu.cs.tweeter.server.service;
 
-import byu.edu.cs.tweeter.server.dao.StatusDAO;
+import byu.edu.cs.tweeter.server.dao.DAOHelperFunctions;
+import byu.edu.cs.tweeter.server.dao.StoryDAO;
 import byu.edu.cs.tweeter.shared.model.domain.service.StoryService;
+import byu.edu.cs.tweeter.shared.net.DataAccessException;
 import byu.edu.cs.tweeter.shared.request.StoryRequest;
 import byu.edu.cs.tweeter.shared.response.StoryResponse;
 
@@ -14,12 +16,28 @@ public class StoryServiceImpl implements StoryService {
         if (request.getLimit() == 0) {
             return new StoryResponse("Bad Request: no statuses requested..");
         }
-
-        return getStatusDAO().getStory(request);
+        String last_datePlusPostedBy;
+        if (request.getLastStatus() != null) {
+            last_datePlusPostedBy = DAOHelperFunctions.createDatePlusPostedBy(
+                    request.getLastStatus().getUser().getAlias(), request.getLastStatus().getTimestamp());
+        }
+        else {
+            last_datePlusPostedBy = null;
+        }
+        try {
+            StoryResponse response = getStoryDAO().getStory(request.getUser(), request.getLimit(), last_datePlusPostedBy);
+            return response;
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+            return new StoryResponse("Error in retrieving story from database..");
+        }
     }
 
-    public StatusDAO getStatusDAO() {
-        return new StatusDAO();
+    private StoryDAO getStoryDAO() {
+        return new StoryDAO();
     }
 
 }
+
+
+
