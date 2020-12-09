392f722cdb878363866bc29c7c3abea4737344f9
issmith962 <is.smith.962@gmail.com>
2020-12-09 02:27:53 -0700

    Added SQS queue handling for UpdateFeed

--- server/src/main/java/byu/edu/cs/tweeter/server/lambda/UpdateFeedHandler.java
+++ server/src/main/java/byu/edu/cs/tweeter/server/lambda/UpdateFeedHandler.java
@@ -2,16 +2,22 @@ package byu.edu.cs.tweeter.server.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
 
 import byu.edu.cs.tweeter.server.service.FeedServiceImpl;
 import byu.edu.cs.tweeter.shared.request.UpdateFeedRequest;
-import byu.edu.cs.tweeter.shared.response.UpdateFeedResponse;
+import byu.edu.cs.tweeter.shared.utils.JsonSerializer;
 
-public class UpdateFeedHandler implements RequestHandler<UpdateFeedRequest, UpdateFeedResponse> {
+public class UpdateFeedHandler implements RequestHandler<SQSEvent, Void> {
     @Override
-    public UpdateFeedResponse handleRequest(UpdateFeedRequest input, Context context) {
+    public Void handleRequest(SQSEvent event, Context context) {
         FeedServiceImpl service = new FeedServiceImpl();
-        UpdateFeedResponse response = service.updateFeed(input);
-        return response;
+        for (SQSMessage msg : event.getRecords()) {
+            UpdateFeedRequest request = JsonSerializer.deserialize(msg.getBody(), UpdateFeedRequest.class);
+            FeedServiceImpl feedService = new FeedServiceImpl();
+            feedService.updateFeed(request);
+        }
+        return null;
     }
 }
