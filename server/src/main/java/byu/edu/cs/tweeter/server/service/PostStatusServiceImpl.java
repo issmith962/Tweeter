295f8f495d6904b72f51777b0ad3b7dace883f76
issmith962 <is.smith.962@gmail.com>
2020-12-09 02:24:10 -0700

    Added functionality for post-status sqs queue

--- server/src/main/java/byu/edu/cs/tweeter/server/service/PostStatusServiceImpl.java
+++ server/src/main/java/byu/edu/cs/tweeter/server/service/PostStatusServiceImpl.java
@@ -2,10 +2,18 @@ package byu.edu.cs.tweeter.server.service;
 
 import byu.edu.cs.tweeter.server.dao.AuthTokenDAO;
 import byu.edu.cs.tweeter.server.dao.DAOHelperFunctions;
+import byu.edu.cs.tweeter.server.dao.PostUpdateFeedMessagesSQSManager;
 import byu.edu.cs.tweeter.server.dao.StoryDAO;
+import byu.edu.cs.tweeter.server.dao.UpdateFeedSQSManager;
+import byu.edu.cs.tweeter.shared.model.domain.Status;
+import byu.edu.cs.tweeter.shared.model.domain.User;
 import byu.edu.cs.tweeter.shared.model.domain.service.PostStatusService;
 import byu.edu.cs.tweeter.shared.net.DataAccessException;
+import byu.edu.cs.tweeter.shared.request.FollowersRequest;
 import byu.edu.cs.tweeter.shared.request.PostStatusRequest;
+import byu.edu.cs.tweeter.shared.request.PostUpdateFeedMessagesRequest;
+import byu.edu.cs.tweeter.shared.request.UpdateFeedRequest;
+import byu.edu.cs.tweeter.shared.response.FollowersResponse;
 import byu.edu.cs.tweeter.shared.response.PostStatusResponse;
 
 public class PostStatusServiceImpl implements PostStatusService {
@@ -40,13 +48,40 @@ public class PostStatusServiceImpl implements PostStatusService {
             return new PostStatusResponse(false, "Error in posting status to story..");
         }
 
-
-        // TODO: Get list of followees
-        // TODO: Create UpdateFeedRequest
-        // TODO: Send UpdateFeedRequest to SQS queue for lambda:UpdateFeed processing
-
+        System.out.println("passed here 1...");
+        // Create PostUpdateFeedMessagesHandler request
+        PostUpdateFeedMessagesRequest postUpdateFeedMessagesRequest = new PostUpdateFeedMessagesRequest(
+                new Status(request.getUser(), request.getPostingTimestamp(), request.getNewStatus())
+        );
+        System.out.println("passed here 2...");
+        // Send message To SQSPostStatusQueue with the request
+        PostUpdateFeedMessagesSQSManager.sendUpdateFeedMessage(postUpdateFeedMessagesRequest);
+        System.out.println("passed here 3...");
 
         return new PostStatusResponse(true);
+    }
+
+    public void sendUpdateFeedMessage(PostUpdateFeedMessagesRequest request) {
+        User lastFollower = null;
+        User followee = request.getStatus().getUser();
+        int limit = 25;
+
+        while (true) {
+            FollowersResponse followersResponse = new FollowerServiceImpl().getFollowers(new FollowersRequest(followee, limit, lastFollower));
+            if (followersResponse.getFollowers() == null) {
+                break;
+            }
+            else if (followersResponse.getFollowers().size() == 0) {
+                break;
+            }
+            else {
+                UpdateFeedRequest updateFeedRequest = new UpdateFeedRequest(request.getStatus(), followersResponse.getFollowers());
+                UpdateFeedSQSManager.updateFeed(updateFeedRequest);
+            }
+            if (!followersResponse.hasMorePages()) {
+                break;
+            }
+        }
     }
 
     public AuthTokenDAO getAuthTokenDAO() {
