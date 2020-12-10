629a59b5d7d428f0e9a787fd76962b71001b3482
issmith962 <is.smith.962@gmail.com>
2020-12-10 05:24:00 -0700

    Created filler program for mass-producing users and follows for passoff

---
+++ server/src/main/java/byu/edu/cs/tweeter/server/dao/dummydatadao/FillerProgram.java
@@ -0,0 +1,8 @@
+package byu.edu.cs.tweeter.server.dao.dummydatadao;
+
+public class FillerProgram {
+    public static void main(String[] args) {
+        Filler filler = new Filler();
+        filler.fillDatabase();
+    }
+}
