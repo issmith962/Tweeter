4a1e8dc993b7c34e90131be92f6efb8b1f293d38
issmith962 <is.smith.962@gmail.com>
2020-12-10 05:38:05 -0700

    deleted

--- server/src/main/java/byu/edu/cs/tweeter/server/dao/dummydatadao/FillerProgram.java
+++
@@ -1,8 +0,0 @@
-package byu.edu.cs.tweeter.server.dao.dummydatadao;
-
-public class FillerProgram {
-    public static void main(String[] args) {
-        Filler filler = new Filler();
-        filler.fillDatabase();
-    }
-}
